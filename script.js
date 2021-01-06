3f22223493ab11479c8c8dc46dbc39b8c8daf508
poweredbylinux <akarshraja@protonmail.com>
2021-01-06 20:59:31 +0530

    Dark Mode improvements

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-// Dark Theme
+$(window).on("load", function () {
+    $(".loader-container").fadeOut("slow");
+});
+AOS.init();
+
+// Dark Mode
+
 let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = $('#dark-mode-toggle');
 const imgReplace = $('#thumbnail-replace-dark');
 
 const enableDarkMode = () => {
-    // Add the class "darkmode" to <body>
+    // Add class "darkmode" to <body>
     $('body').addClass('darkmode');
     // Update darkMode in localStorage
     localStorage.setItem('darkMode', 'enabled');
+    // Change GitHub Pages image
     $(imgReplace).attr('src', '/images/ghpages-dark.png');
+    // Change moon icon to sun icon
+    $(darkModeToggle).html('<i class="fas fa-sun"></i>');
 };
 
 const disableDarkMode = () => {
-    // Remove the class "darkmode" from <body>
+    // Remove class "darkmode" from <body>
     $('body').removeClass('darkmode');
     // Update darkMode in localStorage
     localStorage.setItem('darkMode', null);
+    // Change GitHub Pages image
     $(imgReplace).attr('src', '/images/ghpages.png');
+    // Change sun icon to moon icon
+    $(darkModeToggle).html('<i class="fas fa-moon"></i>');
 };
 
 if (darkMode === 'enabled'){
@@ -37,9 +49,3 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 } else {
     disableDarkMode();
 }
-
-// Other scripts
-$(window).on("load", function () {
-    $(".loader-container").fadeOut("slow");
-});
-AOS.init();
